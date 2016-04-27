2a36b2ef978197a0a3dfe0985b81f8b4c560c18a
ashu94 <singhjug1994@gmail.com>
2016-04-27 11:28:22 +0530

    callback bug resolve

--- app.js
+++ app.js
@@ -1,73 +1,81 @@
-'use strict'
-const http = require('http')
-const Bot = require('messenger-bot')
+'use strict';
+const http = require('http');
+const Bot = require('messenger-bot');
+var request = require('request');
 
 let bot = new Bot({
-  token: 'token',
+  token: 'TOKEN',
   verify: 'VERIFY_TOKEN'
-})
+});
 
-function keywordSeperator(p1) {
-    var str = p1.toLowerCase();
-    str = str.split(" ")
-    str = str[2]
-    var data = {
-        "text":"hello, world!"
+var dynamic_value = function (profile, text, next){
+    var value = {
+        "attachment" :{
+            "type" : "template",
+            "payload" : {
+                "template_type" : "generic",
+                "elements" : []
+            }
+        }
     };
-    var messageData = {
-    "attachment": {
-      "type": "template",
-      "payload": {
-        "template_type": "generic",
-        "elements": [{
-          "title": "Company 1",
-          "subtitle": "Element #1 of an hscroll",
-          "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-          "buttons": [{
-            "type": "web_url",
-            "url": "https://ashutoshksingh.com",
-            "title": "More info"
-          }],
-        },{
-          "title": "Company 2 ",
-          "subtitle": "Element #2 of an hscroll",
-          "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-          "buttons": [{
-            "type": "web_url",
-            "title": "more info",
-            "url": "www.google.com",
-          }],
-        }]
-      }
-    }
-  };
 
-    if (str == "noida"){
-      return JSON.stringify(messageData);
-    } else {
-        return JSON.stringify(data) ;
-      }
-}
+    var dyn_value = [];
+    request('GET URL/text', function (error, response, body) {
+        //Check for error
+        if(error){
+            return console.log('Error:', error);
+        }
+
+        //Check for right status code
+        if(response.statusCode !== 200){
+            return console.log('Invalid Status Code Returned:', response.statusCode);
+        }
+
+        //All is good. Print the body
+        // Show the HTML for the Modulus homepage.
+        var JsonObject= JSON.parse(body);
+        
+        for (var x in JsonObject){
+            let item = {
+              "buttons": []
+            }
+            let buttons_value = {};
+            item ["title"] = JsonObject[x].name;
+            item ["image_url"] = JsonObject[x].imageUrl;
+            item["subtitle"] = JsonObject[x].subTitle;
+            buttons_value["type"] = "web_url";
+            buttons_value["title"] = "More info";
+            buttons_value["url"] = "http://www.google.com"
+            item.buttons.push(buttons_value);
+            dyn_value.push(item);
+
+        };
+
+        value.attachment.payload.elements = dyn_value;
+        next(JSON.stringify(value));
+    });
+};
+
+
 
 bot.on('error', (err) => {
-  console.log(err.message)
-})
+  console.log(err.message);
+});
 
 bot.on('message', (payload, reply) => {
   console.log(payload.message.text);
-  let text = keywordSeperator(payload.message.text);
+  let data = payload.message.text;
+  bot.getProfile(payload.sender.id, (err, profile) => {  
+    dynamic_value(profile, data, function(text){    
+          if (err) throw err;
+          console.log("Got from function: " + text);
+        reply(text, (serr, info) => {
+          if (serr) {
+              console.log(serr);
+          }
+        })
+    });
+  });
+});
 
-  bot.getProfile(payload.sender.id, (err, profile) => {
-    if (err) throw err
-
-    console.log("Got from function: " + text);
-    reply(text, (serr, info) => {
-        if (serr) {
-          console.log(serr);
-}
-        
-    })
-  })
-})
-
-http.createServer(bot.middleware()).listen(3000)
+http.createServer(bot.middleware()).listen(3000);
